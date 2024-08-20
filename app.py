dc90b754e2cb261643016dc80142caaacdbac034
Satyam Sharma <satyam17102003@gmail.com>
2024-08-20 17:00:55 +0530

    Code Refine

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ def clear_cache():
     return 'Cache cleared!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
     
